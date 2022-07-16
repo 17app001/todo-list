8da23c83f923e4fe84f078af02752ea1a29cf4b2
jerrychen <jerry@gmail.com>
2022-07-16 14:17:03 +0800

    完成user功能初始設定

--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile(request):
+    return render(request, './user/profile.html')
+
+def user_register(request):
+    return render(request, './user/register.html')
