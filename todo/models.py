0b46cbc42853b95cff022b68134961e7500855f6
jerrychen <jerry@gmail.com>
2022-07-23 12:03:58 +0800

    1.完成Todo資料表 2.顯示列表功能 3.單一列表資訊

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# title -str
+# text  -str (blank=True)
+# created  -datetime  -(auto=True)
+# date_completed -datetime - (blank=True,null=True)
+# important -bool - (default=False)
+# completed -bool - (default=False)
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(blank=True, null=True)
+    important = models.BooleanField(default=True)
+    completed = models.BooleanField(default=False)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} -{self.title}-{self.created}-({self.user.username})'
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from .models import Todo
 # Create your views here.
 
 
+def view(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    print(todo)
+    return render(request, './todo/view.html', {'todo': todo})
+
+
 def todo(request):
-    return render(request, './todo/todo.html')
+    todos = None
+    if request.user.is_authenticated:
+        todos = Todo.objects.filter(user=request.user)
+
+    return render(request, './todo/todo.html', {'todos': todos})
